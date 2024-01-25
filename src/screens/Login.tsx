6db38aae21f94501cb12960f19b358440ac11036
cynthiaspence <cynthia.spence@world50.com>
2024-01-25 15:31:18 -0500

    Login and sign up UIs

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,15 +1,111 @@
+import { useState } from 'react'
 import {
   SafeAreaProvider,
   SafeAreaView
 } from 'react-native-safe-area-context'
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native'
 
 const Login = () => {
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const handleSubmit = () => {
+    setUsername('')
+    setPassword('')
+  }
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.formContainer}>
+          <Text style={styles.inputLabel}>USERNAME</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={un => setUsername(un)}
+            value={username}
+          />
+          <Text style={styles.inputLabel}>PASSWORD</Text>
+          <TextInput
+            style={styles.input}
+            secureTextEntry
+            onChangeText={pw => setPassword(pw)}
+            value={password}
+          />
+          <Pressable
+            style={styles.submitButton}
+            onPress={handleSubmit}
+          >
+            <Text style={styles.buttonText}>SIGN IN</Text>
+          </Pressable>
+          <Pressable style={styles.switchAuthLink}>
+            <Text style={styles.switchAuthText}>
+              Don't have an account? Sign Up
+            </Text>
+          </Pressable>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#7B2CBF',
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  formContainer: {
+    backgroundColor: '#FFF',
+    borderRadius: 15,
+    padding: 20,
+    borderColor: '#551E85',
+    borderWidth: 5,
+    width: '85%',
+  },
+  inputLabel: {
+    fontWeight: 'bold',
+    color: '#551E85'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  submitButton: {
+    borderRadius: 15,
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#7B2CBF',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#551E85',
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  switchAuthLink: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  switchAuthText: {
+    color: '#551E85',
+    textDecorationLine: 'underline',
+  },
+})
 
 export default Login
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import {
+  Pressable,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native'
 import {
@@ -9,11 +12,52 @@ import {
 } from 'react-native-safe-area-context'
 
 const Register = () => {
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ confPwd, setConfPwd ] = useState('')
+
+  const handleSubmit = () => {
+    setUsername('')
+    setPassword('')
+    setConfPwd('')
+  }
+
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <View style={styles.container}>
-          <Text>Hello!</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.formContainer}>
+          <Text style={styles.inputLabel}>USERNAME</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={un => setUsername(un)}
+            value={username}
+          />
+          <Text style={styles.inputLabel}>PASSWORD</Text>
+          <TextInput
+            secureTextEntry
+            style={styles.input}
+            onChangeText={pw => setPassword(pw)}
+            value={password}
+          />
+          <Text style={styles.inputLabel}>CONFIRM PASSWORD</Text>
+          <TextInput
+            secureTextEntry
+            style={styles.input}
+            onChangeText={cpw => setConfPwd(cpw)}
+            value={confPwd}
+          />
+          <Pressable
+            style={styles.submitButton}
+            onPress={handleSubmit}
+          >
+            <Text style={styles.buttonText}>SIGN UP</Text>
+          </Pressable>
+          <Pressable style={styles.switchAuthLink}>
+            <Text style={styles.switchAuthText}>
+              Already have an account? Sign In
+            </Text>
+          </Pressable>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -22,11 +66,56 @@ const Register = () => {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#7B2CBF',
+    height: '100%',
+    width: '100%',
     display: 'flex',
-    height: 100,
-    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  formContainer: {
     backgroundColor: '#FFF',
-  }
+    borderRadius: 15,
+    padding: 20,
+    borderColor: '#551E85',
+    borderWidth: 5,
+    width: '85%',
+  },
+  inputLabel: {
+    fontWeight: 'bold',
+    color: '#551E85'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  submitButton: {
+    borderRadius: 15,
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#7B2CBF',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#551E85',
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  switchAuthLink: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  switchAuthText: {
+    color: '#551E85',
+    textDecorationLine: 'underline',
+  },
 })
 
 export default Register
