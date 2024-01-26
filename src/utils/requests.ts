1096dad23dfed81c857445fe0afa5d3b06b10bc4
cynthiaspence <cynthia.spence@world50.com>
2024-01-25 23:21:03 -0500

    fix request return values and document code

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -6,7 +6,16 @@ import { useSetRecoilState } from 'recoil'
 import currentUserState from '../recoil/currentUserAtom'
 import currentWordState from '../recoil/currentWordAtom'
 
-// register
+/**
+ * Register new user
+ * return error string if the username is duplicated
+ * set current user and word if successfully created user
+ * toast and return null if 500 response
+ * 
+ * @param username string - user's chosen username
+ * @param password password - user's chosen password
+ * @returns string or null - string error message or null on success or 500
+ */
 export const registerUser = async (username: string, password: string) => {
   const setUserState = useSetRecoilState(currentUserState)
   const setWordState = useSetRecoilState(currentWordState)
@@ -25,7 +34,7 @@ export const registerUser = async (username: string, password: string) => {
     case 201:
       setUserState(() => data.username)
       setWordState(() => data.word)
-      break
+      return null
     case 500:
       Toast.show({
         type: 'error',
@@ -34,11 +43,21 @@ export const registerUser = async (username: string, password: string) => {
         visibilityTime: 2000,
         autoHide: true,
       })
-      break
+      return null
   }
 }
 
-// login
+/**
+ * login user
+ * if username is not found, return error string
+ * if password is incorrect, return error string
+ * on success, set current user and word states, return null
+ * toast and return null if 500 response
+ * 
+ * @param username string - username entered by user
+ * @param password string - password entered by user
+ * @returns string or null - error message string or null on success or 500
+ */
 export const loginUser = async (username: string, password: string) => {
   const setUserState = useSetRecoilState(currentUserState)
   const setWordState = useSetRecoilState(currentWordState)
@@ -60,7 +79,7 @@ export const loginUser = async (username: string, password: string) => {
     case 200:
       setUserState(() => data.username)
       setWordState(() => data.word)
-      break
+      return null
     case 500:
       Toast.show({
         type: 'error',
@@ -69,11 +88,19 @@ export const loginUser = async (username: string, password: string) => {
         visibilityTime: 2000,
         autoHide: true,
       })
-      break
+      return null
   }
 }
 
-// random word
+/**
+ * generates a new word for game play
+ * todo: clear guesses state
+ * sets new word in current word state
+ * toast and return null if 500 response
+ * 
+ * @param username string - current user's username
+ * @returns null
+ */
 export const generateWord = async (username: string) => {
   const setWordState = useSetRecoilState(currentWordState)
   const { status, data } = await axios.get(`${process.env.API_URL}/random-word`, {
@@ -88,6 +115,7 @@ export const generateWord = async (username: string) => {
   switch (status) {
     case 200:
       setWordState(() => data.word)
+      return null
     case 500:
       Toast.show({
         type: 'error',
@@ -96,11 +124,21 @@ export const generateWord = async (username: string) => {
         visibilityTime: 2000,
         autoHide: true,
       })
-      break
+      return null
   }
 }
 
-// complete word
+/**
+ * set word as a completed word
+ * generate a new word
+ * todo: clear guesses state
+ * set new word in recoil state for game play
+ * toast and return null if 500 response
+ * 
+ * @param username string - current user's username
+ * @param word string - successfully completed word
+ * @returns null
+ */
 export const completeWord = async (username: string, word: string) => {
   const setWordState = useSetRecoilState(currentWordState)
   const { status, data } = await axios.post(`${process.env.API_URL}/word-completed`, {
@@ -115,6 +153,7 @@ export const completeWord = async (username: string, word: string) => {
   switch (status) {
     case 200:
       setWordState(() => data.word)
+      return null
     case 500:
       Toast.show({
         type: 'error',
@@ -123,6 +162,6 @@ export const completeWord = async (username: string, word: string) => {
         visibilityTime: 2000,
         autoHide: true,
       })
-      break
+      return null
   }
 }
