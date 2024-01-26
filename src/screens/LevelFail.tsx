525b0ca1caee4cc8fb6ad253425cbca63a9356c2
cynthiaspence <cynthia.spence@world50.com>
2024-01-26 01:17:02 -0500

    level success ui styled and level fail ui no style

--- src/screens/LevelFail.tsx
+++ src/screens/LevelFail.tsx
@@ -2,11 +2,33 @@ import {
   SafeAreaProvider,
   SafeAreaView
 } from 'react-native-safe-area-context'
+import {
+  Pressable,
+  Text,
+  View,
+} from 'react-native'
 
 const LevelFail = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
+        <View>
+          <Text>
+            ROUND OVER
+          </Text>
+          <View>
+            <Pressable>
+              <Text>
+                RETRY
+              </Text>
+            </Pressable>
+            <Pressable>
+              <Text>
+                NEXT
+              </Text>
+            </Pressable>
+          </View>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   )
--- src/screens/LevelSuccess.tsx
+++ src/screens/LevelSuccess.tsx
@@ -1,12 +1,77 @@
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import {
+  SafeAreaProvider,
+  SafeAreaView
+} from 'react-native-safe-area-context'
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
 
 const LevelSuccess = () => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.resultContainer}>
+          <Text
+            style={styles.winText}
+          >
+            YOU WON!
+          </Text>
+          <Pressable style={styles.continueButton}>
+            <Text style={styles.buttonText}>
+              CONTINUE
+            </Text>
+          </Pressable>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#7B2CBF',
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  resultContainer: {
+    backgroundColor: '#FFF',
+    borderRadius: 15,
+    padding: 20,
+    borderColor: '#551E85',
+    borderWidth: 5,
+    width: '85%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  winText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#551E85',
+  },
+  continueButton: {
+    borderRadius: 15,
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#7B2CBF',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#551E85',
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+})
 
 export default LevelSuccess
